0c8d29a9c679218ccc826c8027ce0e11caddaf6f
wun <kevinlay841009@gmail.com>
2024-01-26 11:58:11 +0800

    教練及課程基本建置

---
+++ edit_couerse.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>新增課程</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <?php include("../baseball/ws_css.php") ?>
+    </head>
+
+    <body>
+
+    
+    <?php include("../baseball/ws_js.php") ?>
+    </body>
+</html>
---
+++ teacher.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>教練列表Coach</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <?php include("../baseball/ws_css.php") ?>
+    </head>
+
+    <body>
+       
+
+    <?php include("../baseball/ws_js.php") ?>
+    </body>
+</html>
