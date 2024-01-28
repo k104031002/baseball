533e7b504ad329f950604a121261f68fce65f335
wun <kevinlay841009@gmail.com>
2024-01-28 23:44:43 +0800

    教練個人介面基本版型

--- teacher.php
+++ teacher.php
@@ -1,6 +1,10 @@
 <?php
 require_once("../baseball/db_connect.php");
 
+$sql = "SELECT * FROM teacher WHERE valid=1";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+
 ?>
 
 <!doctype html>
@@ -16,12 +20,18 @@ require_once("../baseball/db_connect.php");
 
 <body>
     <div class="container">
-        <div>
-            
+        <div class="row g-3">
+            <div class="col-md-6">
+                <img class="img-fluid" src="../baseball/assets/img/teacher_img/<?= $row["photo"] ?>" alt="<?= $row["name"] ?>">
+            </div>
+            <div class="col-md-6 border">
+                <h1><?= $row["name"] ?></h1>
+                <div class="border"><?= $row["description"] ?></div>
+            </div>
         </div>
     </div>
 
-    <?php include("../baseball/ws_js.php") ?>
+    <?php include("../baseball/assets/js/ws_js.php") ?>
 </body>
 
 </html>
